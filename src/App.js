fe0eac8fc0d039fc6f707ab09a86df4f548da04a
Matthew H <@hvamy002>
2025-11-04 14:59:47 +1030

    Revert "Transition from JS to React thinking for the music code input"
    This reverts commit 49d31af023ee8a4c3e7874f1671b26d9c1ce08cd.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { StrudelMirror } from '@strudel/codemirror';
 import { evalScope } from '@strudel/core';
 import { drawPianoroll } from '@strudel/draw';
@@ -9,10 +9,6 @@ import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/w
 import { registerSoundfonts } from '@strudel/soundfonts';
 import { stranger_tune } from './tunes';
 import console_monkey_patch, { getD3Data } from './console-monkey-patch';
-import DJButton from './Component/DJButton';
-import PlayResume from './Component/PlayResume';
-import ProcButton from './Component/ProcButton';
-import MusicInput from './Component/MusicInput';
 
 let globalEditor = null;
 
@@ -20,65 +16,54 @@ const handleD3Data = (event) => {
     console.log(event.detail);
 };
 
-//export function SetupButtons() {
+export function SetupButtons() {
 
-//    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
-//    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
-//    document.getElementById('process').addEventListener('click', () => {
-//        Proc()
-//    }
-//    )
-//    document.getElementById('process_play').addEventListener('click', () => {
-//        if (globalEditor != null) {
-//            Proc()
-//            globalEditor.evaluate()
-//        }
-//    }
-//    )
-//}
+    document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
+    document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
+    document.getElementById('process').addEventListener('click', () => {
+        Proc()
+    }
+    )
+    document.getElementById('process_play').addEventListener('click', () => {
+        if (globalEditor != null) {
+            Proc()
+            globalEditor.evaluate()
+        }
+    }
+    )
+}
 
 
 
-//export function ProcAndPlay() {
-//    if (globalEditor != null && globalEditor.repl.state.started == true) {
-//        console.log(globalEditor)
-//        Proc()
-//        globalEditor.evaluate();
-//    }
-//}
+export function ProcAndPlay() {
+    if (globalEditor != null && globalEditor.repl.state.started == true) {
+        console.log(globalEditor)
+        Proc()
+        globalEditor.evaluate();
+    }
+}
 
-//export function Proc() {
+export function Proc() {
 
-//    let proc_text = document.getElementById('proc').value
-//    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
-//    ProcessText(proc_text);
-//    globalEditor.setCode(proc_text_replaced)
-//}
+    let proc_text = document.getElementById('proc').value
+    let proc_text_replaced = proc_text.replaceAll('<p1_Radio>', ProcessText);
+    ProcessText(proc_text);
+    globalEditor.setCode(proc_text_replaced)
+}
 
-//export function ProcessText(match, ...args) {
+export function ProcessText(match, ...args) {
 
-//    let replace = ""
-//    if (document.getElementById('flexRadioDefault2').checked) {
-//        replace = "_"
-//    }
+    let replace = ""
+    if (document.getElementById('flexRadioDefault2').checked) {
+        replace = "_"
+    }
 
-//    return replace
-//}
+    return replace
+}
 
 export default function StrudelDemo() {
 
-    const hasRun = useRef(false);
-    const playCode = () => {
-        globalEditor.evaluate()
-    }
-    const stopCode = () => {
-        globalEditor.stop()
-    }
-
-    const [songText, setSongText] = useState('')
-
-
-
+const hasRun = useRef(false);
 
 useEffect(() => {
 
@@ -114,11 +99,11 @@ useEffect(() => {
             });
             
         document.getElementById('proc').value = stranger_tune
-        //SetupButtons()
-        //Proc()
+        SetupButtons()
+        Proc()
     }
-    globalEditor.setCode(songText);
-}, [songText]);
+
+}, []);
 
 
 return (
@@ -129,14 +114,17 @@ return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                        <MusicInput defaultValue={songText} onChange={ (e) => setSongText(e.target.value)} />
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
+                        <textarea className="form-control" rows="15" id="proc" ></textarea>
                     </div>
                     <div className="col-md-4">
 
                         <nav>
-                            <ProcButton />
+                            <button id="process" className="btn btn-outline-primary">Preprocess</button>
+                            <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
                             <br />
-                            <PlayResume onPlay={playCode} onStop={stopCode} />
+                            <button id="play" className="btn btn-outline-primary">Play</button>
+                            <button id="stop" className="btn btn-outline-primary">Stop</button>
                         </nav>
                     </div>
                 </div>
@@ -145,9 +133,19 @@ return (
                         <div id="editor" />
                         <div id="output" />
                     </div>
-                    
                     <div className="col-md-4">
-                        <DJButton />
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
+                            <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                p1: ON
+                            </label>
+                        </div>
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
+                            <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                p1: HUSH
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
