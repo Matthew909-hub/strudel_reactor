6ec69a2eddc220e57de913def3a05f27fb6f7075
Matthew H <@hvamy002>
2025-11-05 11:42:46 +1030

    Added features - Tempo (CPM) - Volume scroller - Check radio

--- src/Component/DJButton.jsx
+++ src/Component/DJButton.jsx
@@ -1,18 +1,35 @@
 function DJButton() {
     return (
         <>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">Tempo</span>
+                <input type="text" class="form-control" id="cpm_text_inbput" placeholder="120" aria-label="Username" aria-describedby="cpm_label"/>
+            </div>
+            <div>
+                <label for="volume range" className="form-label text-white">Adjust Music Volume</label>
+                <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
+            </div>
+
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                <label className="form-check-label text-white" htmlFor="flexRadioDefault1">
-                    p1: ON
+                <input className="form-check-input" type="checkbox" value="" id="bassline"/>
+                <label className="form-check-label text-white" for="bassline">
+                    Baseline
                 </label>
             </div>
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                <label className="form-check-label text-white" htmlFor="flexRadioDefault2">
-                    p1: HUSH
+                <input className="form-check-input" type="checkbox" value="" id="drums" />
+                <label className="form-check-label text-white" for="drums">
+                    Drums
                 </label>
             </div>
+            <div className="form-check">
+                <input className="btn-check" type="checkbox" value="" id="drums2" autocomplete="off" />
+                <label className="btn text-white" for="drums2">
+                    Drums 2
+                </label>
+            </div>
+
+
         </>
   );
 }
