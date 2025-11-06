b5e3e9c38cdadcd29d326403a8cb09ac9eea6f01
Matthew H <@hvamy002>
2025-11-06 16:07:43 +1030

    Added htmlFor and className

--- src/Component/DJButton.jsx
+++ src/Component/DJButton.jsx
@@ -2,30 +2,30 @@ function DJButton() {
     return (
         <>
             <div className="input-group mb-3">
-                <span className="input-group-text" id="cpm_label">Tempo</span>
-                <input type="text" class="form-control" id="cpm_text_inbput" placeholder="120" aria-label="Username" aria-describedby="cpm_label"/>
+                <span className="input-group-text" id="cpm_label">setcpm</span>
+                <input type="text" className="form-control" id="cpm_text_inbput" placeholder="120" aria-label="Username" aria-describedby="cpm_label"/>
             </div>
             <div>
-                <label for="volume range" className="form-label text-white">Adjust Music Volume</label>
+                <label htmlFor="volume range" className="form-label text-white">Adjust Music Volume</label>
                 <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
             </div>
 
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="bassline"/>
-                <label className="form-check-label text-white" for="bassline">
+                <label className="form-check-label text-white" htmlFor="bassline">
                     Baseline
                 </label>
             </div>
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="drums" />
-                <label className="form-check-label text-white" for="drums">
+                <label className="form-check-label text-white" htmlFor="drums">
                     Drums
                 </label>
             </div>
             <div className="form-check">
-                <input className="btn-check" type="checkbox" value="" id="drums2" autocomplete="off" />
-                <label className="btn text-white" for="drums2">
-                    Drums 2
+                <input className="form-check-input" type="checkbox" value="" id="drums2" />
+                <label className="form-check-label text-white" htmlFor="drums2">
+                    Drums2
                 </label>
             </div>
 
